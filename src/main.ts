066d014810af3a746cb0a56fa4b620f45bf86d68
Vikash KK <hello@vikashkk.com>
2026-04-19 00:21:41 +0530

    Update

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: '*' });
+  app.enableCors({
+    origin: ['http://localhost:5173', 'https://challan.vikashkk.com'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true,
+  });
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(
     new ValidationPipe({
