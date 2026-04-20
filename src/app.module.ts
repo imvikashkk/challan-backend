60be00f1e0b614e4eb910d840c309cb63f409f14
Vikash KK <hello@vikashkk.com>
2026-04-21 01:10:06 +0530

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,34 @@
-import { Module, } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from "@nestjs/config"
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ChallanModule } from './challan/challan.module';
 import { VehicleModule } from './vehicle/vehicle.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: ".env"
+      envFilePath: '.env',
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: "postgres",
-        database: configService.get<string>("DATABASE_NAME"),
-        username: configService.get<string>("DATABASE_USERNAME"),
-        password: configService.get<string>("DATABASE_PASSWORD"),
-        host: configService.get<string>("DATABASE_HOST"),
-        port: configService.get<number>("DATABASE_PORT"),
+        type: 'postgres',
+        database: configService.get<string>('DATABASE_NAME'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         synchronize: true,
         extra: {
           max: 20,
           idleTimeoutMillis: 30000,
         },
-      })
+      }),
     }),
     UserModule,
     ChallanModule,
@@ -37,6 +37,4 @@ import { VehicleModule } from './vehicle/vehicle.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-
-}
+export class AppModule {}
